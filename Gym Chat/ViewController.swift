5da1a345d4bb5c3ad277997f12972097c2d8461e
phoebe <phoebe@phoebe2019.wv.cc.cmu.edu>
2019-09-14 06:38:12 -0400

    my bio screen

---
+++ Gym Chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gym Chat
+//
+//  Created by phoebe on 9/14/19.
+//  Copyright © 2019 VIMP. All rights reserved.
+//
+
+import UIKit
+
+class friendsController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
